06924107407e4a52baeb54509f7b849a43b00cb1
Marshall <363752922@qq.com>
2021-06-14 12:18:34 -0400

    Exercise5

---
+++ Exercise5/ComplexNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    class ComplexNumber
+    {
+        public int Real { get; set; }
+        public int Imaginary { get; set; }
+        public ComplexNumber(int real,int imaginary)
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+        public new string ToString()
+        {
+            return "(" +Convert.ToString(this.Real)+","+Convert.ToString(this.Imaginary)+")";
+
+        }
+        public void SetImaginary(int imaginary)
+        {
+            this.Imaginary = imaginary;
+        }
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(this.Real * this.Real + this.Imaginary * this.Imaginary);
+        }
+        public ComplexNumber Add (ComplexNumber a)
+        {
+            this.Real = a.Real + this.Real;
+            this.Imaginary = a.Imaginary + this.Imaginary;
+            return this;
+        }
+    }
+}
